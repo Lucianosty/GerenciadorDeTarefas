31fcab231641f4981d6c543fb26a084e7260568d
Lucianosty <163857383+Lucianosty@users.noreply.github.com>
2025-05-16 10:25:38 -0300

    Add files via upload

---
+++ Gerenciamento_de_Tarefas/Gerenciamento_de_Tarefas/Form1.cs
@@ -0,0 +1,78 @@
+namespace Gerenciamento_de_Tarefas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inicio()
+        {
+            FormComeco inicio = new FormComeco();
+            inicio.TopLevel = false;
+            inicio.FormBorderStyle = FormBorderStyle.None;
+            inicio.Dock = DockStyle.Fill;
+            panel1.Controls.Clear();
+            panel1.Controls.Add(inicio);
+            inicio.Show();
+        }
+
+        private void NovaTarefa()
+        {
+            FormNovaTarefa NovaTarefa = new FormNovaTarefa();
+            NovaTarefa.TopLevel = false;
+            NovaTarefa.FormBorderStyle = FormBorderStyle.None;
+            NovaTarefa.Dock = DockStyle.Fill;
+            panel1.Controls.Clear();
+            panel1.Controls.Add(NovaTarefa);
+            NovaTarefa.Show();
+        }
+
+        private void TarefasPendentes()
+        {
+            FormTarefasPendentes TarefasPendentes = new FormTarefasPendentes();
+            TarefasPendentes.TopLevel = false;
+            TarefasPendentes.FormBorderStyle = FormBorderStyle.None;
+            TarefasPendentes.Dock = DockStyle.Fill;
+            panel1.Controls.Clear();
+            panel1.Controls.Add(TarefasPendentes);
+            TarefasPendentes.Show();
+        }
+
+        private void btnNovaTarefa_Click(object sender, EventArgs e)
+        {
+            NovaTarefa();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            inicio();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTarefasPen_Click(object sender, EventArgs e)
+        {
+            TarefasPendentes();
+        }
+    }
+}
---
+++ Gerenciamento_de_Tarefas/Gerenciamento_de_Tarefas/FormComeco.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Gerenciamento_de_Tarefas
+{
+    partial class FormComeco
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 450);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            label1.Location = new Point(185, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(424, 196);
+            label1.TabIndex = 0;
+            label1.Text = "Seja bem vindo ao melhor sistema de gerencimento de tarefas\r\n\r\nCrie e gerencie suas tarefas pendentes\r\n\r\n";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // FormComeco
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Name = "FormComeco";
+            Text = "FormComeço";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+    }
+}
